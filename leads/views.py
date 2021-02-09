627b72bdff7a7fab47313f6a1ad403a99b64cb57
ORuan <ruanpablomp@gmail.com>
2021-02-09 11:40:24 -0300

    fix cancel register error

--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,7 @@ from leads.forms import UserForm, UserDelForm
 from django.core.exceptions import ObjectDoesNotExist
 import logging
 
+
 def register_users(request):
     if request.method == "POST":
         form_data = UserForm(request.POST)
@@ -28,22 +29,10 @@ def cancel_users(request):
     context = dict()
     if request.method == "POST":
         form_del = UserDelForm(request.POST)
-        if form_del.is_valid():
-            try:
-                lead_instance = Leads.objects.get(number=form_del.cleaned_data['number'])
-                if lead_instance:
-                    context['erros'] = "Número não encontrado"
-                    return render(request, 'forms/cancel.html', context)
-                lead_instance.delete()
-                return redirect('leads:sucess')
-            except Exception as err:
-                logging.error(err)
-                context['form'] = UserDelForm(request.POST)
-                return render(request, 'forms/cancel.html', context)
-
-        else:
-            context['form'] = UserDelForm(request.POST) 
-            return render(request, 'forms/cancel.html', context)
+        lead_instance = Leads.objects.get(
+            number=request.POST['number'])
+        lead_instance.delete()
+        return redirect('leads:sucess')
     else:
         context['form'] = UserDelForm()
         return render(request, 'forms/cancel.html', context)
